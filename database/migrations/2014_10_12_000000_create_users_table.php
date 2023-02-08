2966ac3bb68e790663d98dc41f6549bc3945426f
Miguel Madrigal <miguel.madrigal7676@alumnos.udg.mx>
2023-02-07 19:04:14 -0600

    actualizacion del repo

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,13 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->id();                        // Integer Unsigned Increment
+            $table->string('name');              // Varchar ('name', tamaño)
+            $table->string('email')->unique();   // Varchar ('email', tamaño) Unique
+            $table->timestamp('email_verified_at')->nullable(); // Timestamp ('email_verified_at'), puede ser nulo
+            $table->string('password');          // Varchar ('password', tamaño) 
+            $table->rememberToken();             // Varchar ('remember_token', tamaño->100), utilizado para recordar la sesión del usuario
+            $table->timestamps();                // Timestamps (created_at, updated_at), cuando se crean registros
         });
     }
 
